042e65117a1b771854bf159bf38a1370c487ec84
shashikhanal <khanalshashi@gmail.com>
2017-02-21 23:39:13 +0545

    inserting node in linked list - at the beginning, after a node

--- linkedList-1.cpp
+++ linkedList-1.cpp
@@ -11,6 +11,32 @@ struct node
 	struct node *next;
 };
 
+// Push to the front of the linked list
+void appendAtBeginning(struct node *head, struct node **beforeHead_ref, int new_data)
+{
+	struct node *new_node = (struct node *) malloc(sizeof(struct node));
+
+	new_node->data = new_data;
+	new_node->next = head;
+	(*beforeHead_ref) = new_node;
+	cout<<"beforeHead data: "<<new_node->data<<" next: "<<new_node->next;
+}
+
+void appendAfter(struct node *prev_node, int new_data)
+{
+	if (prev_node == NULL) {
+		printf("The given previous node cannot be NULL");
+		return;
+	}
+
+	struct node *new_node = (struct node *) malloc(sizeof(struct node));
+
+	new_node->data = new_data;
+	new_node->next = prev_node->next;
+	prev_node->next = new_node;
+	cout<<"\nNew node to be added after: "<<new_node->data<<" next: "<<new_node->next;
+}
+
 void printList(struct node *n)
 {
 	while(n != NULL) 
@@ -56,8 +82,21 @@ int main()
 	cout<< "second :"<< "data :" << second->data <<" next: " << second->next<<endl;
 	cout<< "third :" << "data :" << third->data <<" next: " << third->next<<endl;
 	cout<<endl;
-	
-	printList(head);
+
+	struct node *beforeHead = NULL;
+	appendAtBeginning(head, &beforeHead, 100);
+
+	printf("\nUpdated Linked List is: ");
+	printList(beforeHead);
+	cout<<endl;
+
+	// Adding after second node
+	struct node *afterNode = NULL;
+	appendAfter(second, 200);
+
+	printf("\nUpdated Linked List is: ");
+	printList(beforeHead);		
+	cout<<endl;
 
 	return 0;
 }
